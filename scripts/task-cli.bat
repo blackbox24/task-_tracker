1de3f1d18a5769b452654f0a44a633b8057c8077
blackbox24 <nicardid@gmail.com>
2025-03-07 13:13:40 +0000

    chore: created scripts

---
+++ scripts/task-cli.bat
@@ -0,0 +1,30 @@
+@echo off
+
+py ./../main.py %1 %2 %3
+@REM  task-cli   [action] [description]
+@REM  task-cli   [action] [index] [description]
+@REM  task-cli   [action] [index]
+@REM  task-cli   [action] 
+@REM  task-cli   [action] [status]
+
+
+
+@REM Examples
+@REM  task-cli add "Buy groceries"
+@REM # Output: Task added successfully (ID: 1)
+
+@REM # Updating and deleting tasks
+@REM task-cli update 1 "Buy groceries and cook dinner"
+@REM task-cli delete 1
+
+@REM # Marking a task as in progress or done
+@REM task-cli mark-in-progress 1
+@REM task-cli mark-done 1
+
+@REM # Listing all tasks
+@REM task-cli list
+
+@REM # Listing tasks by status
+@REM task-cli list done
+@REM task-cli list todo
+@REM task-cli list in-progress  
